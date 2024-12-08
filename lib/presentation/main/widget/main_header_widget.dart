4ef573366a6d1be363754f9a50e38c5bb9d6f33c
seoyerin <us3544974@gmail.com>
2024-12-08 22:23:28 +0900

    FEAT ::

---
+++ lib/presentation/main/widget/main_header_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainHeaderWidget extends StatefulWidget {
+  const MainHeaderWidget({super.key});
+
+  @override
+  State<MainHeaderWidget> createState() => _MainHeaderWidgetState();
+}
+
+class _MainHeaderWidgetState extends State<MainHeaderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
