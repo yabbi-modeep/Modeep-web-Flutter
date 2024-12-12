6cc4a8cdc3232b37757811d513ca75673ce9bb1b
seoyerin <us3544974@gmail.com>
2024-12-12 19:39:21 +0900

    CHORE ::

---
+++ lib/core/modeep_color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MoDeepColor{
+  static Color red = const Color(0xFFFF6363);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+
+  static Color main01 = const Color(0xFF00FF32);
+  static Color main02 = const Color(0xFF38D953);
+  static Color main03 = const Color(0xFF41DD5E);
+  static Color main04 = const Color(0xFF63E67D);
+  static Color main05 = const Color(0xFF48E166);
+  static Color main06 = const Color(0xFFC3FFC2);
+
+  static Gradient main = LinearGradient(
+    colors: [Colors.black, Color(0xFF27FF47)],
+    begin: Alignment(0.00, -1.00),
+    end: Alignment(0, 1),
+  );
+
+}
