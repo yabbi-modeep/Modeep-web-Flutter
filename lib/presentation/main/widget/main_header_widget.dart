92922eadacb5f15bcba491c25ffaa5362271c903
seoyerin <us3544974@gmail.com>
2024-12-12 19:39:56 +0900

    REFACTOR ::

--- lib/presentation/main/widget/main_header_widget.dart
+++ lib/presentation/main/widget/main_header_widget.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:modeepweb/core/modeep_color.dart';
 
-class MainHeaderWidget extends StatefulWidget {
+class MainHeaderWidget extends StatelessWidget {
   const MainHeaderWidget({super.key});
 
   @override
-  State<MainHeaderWidget> createState() => _MainHeaderWidgetState();
-}
-
-class _MainHeaderWidgetState extends State<MainHeaderWidget> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SliverAppBar(
+      expandedHeight: 90.0,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Container(
+          color: MoDeepColor.black,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, top: 17),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: GestureDetector(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/images/logo/modeeptitle.png',
+                  width: 65,
+                  height: 42,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      pinned: false,
+      floating: false,
+    );
   }
 }
